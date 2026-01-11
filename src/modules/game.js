60da77e309895e5b5c589ca53b7e56edb7276334
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-11 13:48:16 +0800

    feat(game): track current player, add getter function

--- src/modules/game.js
+++ src/modules/game.js
@@ -3,10 +3,14 @@ import { createPlayer } from './player.js';
 function createGame() {
   const player1 = createPlayer('Player 1', false);
   const player2 = createPlayer('Computer', true);
+  let currentPlayer = player1;
 
   return {
     player1: player1,
     player2: player2,
+    getCurrentPlayer() {
+      return currentPlayer;
+    },
   };
 }
 
