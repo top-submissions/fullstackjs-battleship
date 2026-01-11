d730fbd2c3ab4a929e58dfe2d19f0c35550f12bc
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-11 13:34:15 +0800

    test(player): can attack enemy board

--- src/test/player.test.js
+++ src/test/player.test.js
@@ -1,5 +1,6 @@
 import { createPlayer } from '../modules/player.js';
 import { createGameboard } from '../modules/gameboard.js';
+import { createShip } from '../modules/ship.js';
 
 describe('player module', () => {
   describe('player factory', () => {
@@ -9,6 +10,17 @@ describe('player module', () => {
       expect(player.gameboard).toBeDefined();
       expect(typeof player.gameboard.placeShip).toBe('function');
       expect(typeof player.gameboard.receiveAttack).toBe('function');
+    });
+
+    test('player can attack enemy gameboard', () => {
+      const player = createPlayer('Player 1');
+      const enemy = createPlayer('Player 2');
+      const ship = createShip(3);
+
+      enemy.gameboard.placeShip(ship, [0, 0], 'horizontal');
+      player.attack(enemy.gameboard, [0, 0]);
+
+      expect(ship.hits).toBe(1);
     });
   });
 });
